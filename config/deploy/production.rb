3d6b09d01f46046ddcc4ccba15b874e1233beab3
Barbu <bbsoft0@gmail.com>
2016-10-05 11:02:16 -0400

    updated production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,6 @@ server '188.166.163.34', user: 'root', roles: %w{web app db}
 # only if your app already has a domain, then add:
 # set :nginx_server_name, 'mydomain.com'
 
+config.serve_static_assets = true
+config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+config.assets.compile = true
